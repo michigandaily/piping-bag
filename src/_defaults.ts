8a1337fa56f7765146ef094cb8e505d12c89bc30
yumarie <yumarie@umich.edu>
2026-04-23 02:12:03 -0400

    reconstruct defaults

---
+++ src/_defaults.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_REGION = "us-east-2"
+export enum RUNTIME {
+  DEFAULT_NODEJS = "nodejs24.x",
+  DEFAULT_PYTHON = "python3.14"
+}
+export enum BUNDLE {
+  DEFAULT_NODE_TARGET = "node24"
+}
