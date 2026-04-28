33dfc009442919b63bcd1439fdbc15c00b2092dd
yumarie <yumarie@umich.edu>
2026-04-28 04:37:23 -0400

    plan out supported pipe commands

--- src/pipe.ts
+++ src/pipe.ts
@@ -6,9 +6,18 @@ program
   .name("pipe")
   .description("Data collection scripts")
   .command("deploy", "Deploys a custom script to AWS Lambda")
+  .command("upload", "Uploads a zipped script to AWS Lambda")
   .command(
     "schedule",
     "Schedules a deployed AWS Lambda function to run at a specific rate",
+  )
+  .command(
+    "list",
+    "Lists all instantiated lambdas and schedules made by piping-bag",
+  )
+  .command(
+    "destroy",
+    "Destroys all global AWS resources related to the piping-bag configuration",
   );
 
 program.parse(process.argv);
